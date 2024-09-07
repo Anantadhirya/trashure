1089afe3066845e9bc37a2840782065acc94edd3
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2024-09-07 18:10:16 +0700

    feat: create class Chat

---
+++ Chat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trashure
+{
+    public class Chat
+    {
+        private int _chatID;
+        private int _senderID;
+        private int _receiverID;
+        private string _message;
+        private DateTime _timeSent;
+
+        public Chat(int chatID, int senderID, int receiverID, string message, DateTime time)
+        {
+            _chatID = chatID;
+            _senderID = senderID;
+            _receiverID = receiverID;
+            _message = message;
+            _timeSent = time;
+        }
+
+        publid addMessage(string message)
+        {
+            _message = message;
+        }
+    }
+}
