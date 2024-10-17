bd1370efb3f096ee36fafad531b8418b741b5f1a
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2024-10-17 17:05:48 +0700

    Feat: Tambah Sampah Page

--- pages/TambahSampahPage.xaml.cs
+++ pages/TambahSampahPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,16 @@ namespace trashure.pages
         public TambahSampahPage()
         {
             InitializeComponent();
+        }
+
+        private void onTambahBarang(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void onTambahGambar(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
