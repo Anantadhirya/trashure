dc26d3d3afb96806726b0257c4cb1cdf137b7aa3
Anantadhirya <anantadhirya@gmail.com>
2024-10-26 14:29:19 +0700

    feat: hash helper function & package

---
+++ components/HashHelper.cs
@@ -0,0 +1,35 @@
+﻿using Konscious.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trashure.components
+{
+    internal class HashHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (var hasher = new Argon2id(Encoding.UTF8.GetBytes(password)))
+            {
+                hasher.DegreeOfParallelism = 8;
+                hasher.MemorySize = 65536;
+                hasher.Iterations = 4;
+                byte[] hashBytes = hasher.GetBytes(32);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            try
+            {
+                return HashPassword(password) == hashedPassword;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
